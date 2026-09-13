483ab1c725325cf918621067397a47b804ca23c0
DrKapable <jumamustafaphiri@gmail.com>
2026-09-13 18:52:02 +0200

    Add MedMinds Content Studio page

---
+++ app/admin/content/page.tsx
@@ -0,0 +1,7 @@
+import { MedMindsContentStudio } from "@/components/medminds-content-studio";
+
+export const dynamic = "force-dynamic";
+
+export default function ContentStudioPage() {
+  return <MedMindsContentStudio />;
+}
